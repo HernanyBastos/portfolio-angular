124d0e9424162e6b1cbd9299eabab18d41afe309
Hernany Bastos <hernanybastosfig@gmail.com>
2024-11-28 14:40:26 -0300

    Finalizando o projeto

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 //Interfaces
 import { IProjects } from '../interfaces/IProjects.interface';
 
+//Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+//Enum
+import { EDialogPanelClass } from '../enum/EDialogPanelClass.enum';
+
+//Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -25,6 +35,13 @@ export class ProjectsComponent {
           href: "https://vidafullstack.com.br",
         }
       ]
-    }
+    },
   ])
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
